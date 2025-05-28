81fdfdd15b68963430a9cac79437f4a55101e21f
Kelly Norton <kellegous@gmail.com>
2025-05-27 20:50:04 -0400

    remove unneeded lambda

--- c++/0005-longest-palindromic-substring.cc
+++ c++/0005-longest-palindromic-substring.cc
@@ -5,6 +5,16 @@
 
 namespace longestPalindromicSubstring {
 
+std::pair<int, int> expand_around_center(
+    const std::string_view& s, int left, int right
+) {
+  while (left >= 0 && right < s.length() && s[left] == s[right]) {
+    left--;
+    right++;
+  }
+  return {left + 1, right - 1};
+}
+
 std::string solve(const std::string_view& s) {
   if (s.empty()) {
     return "";
@@ -13,18 +23,9 @@ std::string solve(const std::string_view& s) {
   int start = 0;
   int max_len = 1;
 
-  // Helper function to expand around center
-  auto expand_around_center = [&s](int left, int right) -> std::pair<int, int> {
-    while (left >= 0 && right < s.length() && s[left] == s[right]) {
-      left--;
-      right++;
-    }
-    return {left + 1, right - 1};
-  };
-
   for (int i = 0; i < s.length(); ++i) {
     // Check for odd length palindromes
-    auto [l1, r1] = expand_around_center(i, i);
+    auto [l1, r1] = expand_around_center(s, i, i);
     int len1 = r1 - l1 + 1;
     if (len1 > max_len) {
       max_len = len1;
@@ -32,7 +33,7 @@ std::string solve(const std::string_view& s) {
     }
 
     // Check for even length palindromes
-    auto [l2, r2] = expand_around_center(i, i + 1);
+    auto [l2, r2] = expand_around_center(s, i, i + 1);
     int len2 = r2 - l2 + 1;
     if (len2 > max_len) {
       max_len = len2;
