96043fbbcac50df0b32bcfa2b06bca2c7fdc4830
Kelly Norton <kellegous@gmail.com>
2025-06-18 06:51:50 -0400

    char array

--- swift/Sources/LeetCode/0005LongestPalindromeSubstring.swift
+++ swift/Sources/LeetCode/0005LongestPalindromeSubstring.swift
@@ -1,14 +1,15 @@
 public func longestPalindromeSubstring(_ s: String) -> String {
-    guard !s.isEmpty else {
-        return ""
-    }
+    guard !s.isEmpty else { return "" }
     
-    var start = 0;
-    var maxLength = 1;
+    var start = 0
+    var maxLength = 1
+
+    // avoid looking for the start of chars w/ index
+    let chars = Array(s)
     
     for i in 0..<s.count {
         // check for odd length palindromes
-        let (l1, r1) = expandAroundCenter(s, left: i, right: i)
+        let (l1, r1) = expandAroundCenter(chars, left: i, right: i)
         let len1 = r1 - l1 + 1
         if len1 > maxLength {
             maxLength = len1
@@ -16,7 +17,7 @@ public func longestPalindromeSubstring(_ s: String) -> String {
         }
        
         // check for even length palindromes
-        let (l2, r2) = expandAroundCenter(s, left: i, right: i+1)
+        let (l2, r2) = expandAroundCenter(chars, left: i, right: i+1)
         let len2 = r2 - l2 + 1
         if len2 > maxLength {
             maxLength = len2
@@ -28,11 +29,10 @@ public func longestPalindromeSubstring(_ s: String) -> String {
     return String(s[startIdx..<s.index(startIdx, offsetBy: maxLength)])
 }
 
-func expandAroundCenter(_ s: String, left: Int, right: Int) -> (Int, Int) {
+func expandAroundCenter(_ s: [Character], left: Int, right: Int) -> (Int, Int) {
     var left = left;
     var right = right;
-    while left >= 0 && right < s.count &&
-            s[s.index(s.startIndex, offsetBy: left)] == s[s.index(s.startIndex, offsetBy: right)] {
+    while left >= 0 && right < s.count && s[left] == s[right] {
         left -= 1
         right += 1
     }
